c8603c850eccb50604986991f8d532e6cd6afdb8
huios <services.huios@gmail.com>
2018-12-04 14:01:27 +0100

    feat: blog

---
+++ src/main/java/com/huios/blog/repository/TagRepository.java
@@ -0,0 +1,15 @@
+package com.huios.blog.repository;
+
+import com.huios.blog.domain.Tag;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ * Spring Data  repository for the Tag entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+
+}
--- src/main/java/com/huios/blog/security/AuthoritiesConstants.java
+++ src/main/java/com/huios/blog/security/AuthoritiesConstants.java
@@ -8,6 +8,12 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
+    
+    public static final String PRATICIEN = "ROLE_PRATICIEN";
+    
+    public static final String PAYEUR = "ROLE_PAYEUR";
+    
+    public static final String PATIENT = "ROLE_PATIENT";
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
